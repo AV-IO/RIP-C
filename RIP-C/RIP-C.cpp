ea1141aea4ffa56f5887faa40ef66d1de476633b
Ian Guile <ianguile513@gmail.com>
2015-10-20 04:49:45 -0700

    (pre-debug)

--- RIP-C/RIP-C.cpp
+++ RIP-C/RIP-C.cpp
@@ -4,7 +4,11 @@
 #include <locale>
 #include <codecvt>
 
-
+// Created by AV-IO
+// Program replaces all semi-colons in c/c++/c# filetypes with greek question marks.
+// These two characters look identical, but a compiler will not accept a greek question mark.
+// Use responsibly (or not, it's up to you)
+// It's probably not wise to run this on C:/ or root
 
 bool is_c_extension(std::string s) {
 	for (unsigned long i = s.length() - 2; i > s.length() - 4; --i) {
@@ -19,16 +23,16 @@ bool is_c_extension(std::string s) {
 
 int main() {
 	std::experimental::filesystem::file_status fStatus;
-	std::ifstream i_file; //original file
-	std::wofstream o_file; // created file
+	std::ifstream i_file; //Original file
+	std::wofstream o_file; //Created file
 	for (auto& rdi : std::experimental::filesystem::recursive_directory_iterator(std::experimental::filesystem::current_path())) {
-		std::experimental::filesystem::path pth = rdi;
-		fStatus = std::experimental::filesystem::status(pth);
-		if (std::experimental::filesystem::is_regular_file(fStatus) && is_c_extension(pth.string())) {
+		std::experimental::filesystem::path pth = rdi; //Creating path object from iterator
+		fStatus = std::experimental::filesystem::status(pth); //Getting status (for directory/file determination) of current item in directory
+		if (std::experimental::filesystem::is_regular_file(fStatus) && is_c_extension(pth.string())) { //If item in directory is a file, and has an extension that coincides with common c/c++/c# file types
 			std::experimental::filesystem::rename(pth.string(), pth.string() + "1");
 			i_file.open(pth.string() + "1", std::ios::in);
 			o_file.open(pth.string(), std::ios::out);
-			o_file.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>()));
+			o_file.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>())); //UTF-8 compatible output
 			std::string current_line;
 			while (!i_file.eof()) {
 				std::getline(i_file, current_line);
@@ -36,14 +40,14 @@ int main() {
 					if (current_line[i] != ';') {
 						o_file << current_line[i];
 					}
-					else {
+					else { //If current character is a semi-colon, replace with greek question mark
 						o_file << L"\uCDBE";
 					}
 				}
 			}
 			i_file.close();
 			o_file.close();
-			std::experimental::filesystem::remove(pth.string() + "1");
+			std::experimental::filesystem::remove(pth.string() + "1"); // deleting original file
 		}
 	}
 	return 0;
